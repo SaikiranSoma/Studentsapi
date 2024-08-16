66831017e95e30480a4a248df5af6cfebeff9fb6
Saikiran.Soma <saikiran.soma@ust.com>
2024-08-16 17:36:40 +0530

    Added Studentdb context

---
+++ StudentApi/Data/StudentDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentApi.Models;
+
+namespace StudentApi.Data
+{
+    public class StudentDbContext:DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options) { }
+
+        public DbSet<Student> students { get; set; }
+
+    }
+}
